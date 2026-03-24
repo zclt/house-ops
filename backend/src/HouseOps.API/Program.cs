cbdadc3622a6b416ac37fdf95e92bde874dc402a
Matheus Zucolotto <matheus.zclt@gmail.com>
2026-03-24 08:37:10 -0300

    feat: Add postgres on docker compose

--- backend/src/HouseOps.API/Program.cs
+++ backend/src/HouseOps.API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 
 // Database configuration
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? "Host=localhost;Database=house_ops;Username=postgres;Password=password";
+    ?? "Host=localhost;Database=houseops_db;Username=user;Password=password";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
